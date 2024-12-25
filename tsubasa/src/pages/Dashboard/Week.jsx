a92862387fe930531bfdfaceb7b34f0eaafe468f
PQ-007 <bilguuntushig.a@gmail.com>
2024-12-25 22:39:55 +0800

    firebase okeyy

--- tsubasa/src/pages/Dashboard/Week.jsx
+++ tsubasa/src/pages/Dashboard/Week.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { ArrowLeft, ArrowRight, ChevronLeft, ChevronRight } from "lucide-react";
 import React, { useState } from "react";
 
 const Week = () => {
@@ -35,37 +36,38 @@ const Week = () => {
   const today = new Date();
 
   return (
-    <div className="py-2 font-sans ">
+    <div className="py-5 font-sans ">
       {/* Header */}
       <div className="flex items-center justify-between">
-        <h2 className="text-xl font-semibold">
-          {currentWeek[0].toLocaleString("default", { month: "long" }).slice(0,3)}{" "}
+        <h2 className="text-2xl font-semibold">
+          {currentWeek[0]
+            .toLocaleString("default", { month: "long" })
+            .slice(0, 3)}{" "}
           {currentWeek[0].getFullYear()}
         </h2>
         <div className="flex items-center space-x-2">
-          <Button 
-            className="p-1 rounded-md border border-gray-300"
+          <Button
+            className={"h-8 w-8 rounded-full"}
             onClick={() => changeWeek(-1)}
           >
-            <ChevronLeft/>
+            <ArrowLeft className="h-4 w-4" />
+            <span className="sr-only">Next slide</span>
           </Button>
           <Button
-            className="p-1 rounded-md border border-gray-300"
+            className={"h-8 w-8 rounded-full"}
             onClick={() => changeWeek(1)}
           >
-            <ChevronRight/>
+            <ArrowRight className="h-4 w-4" />
+            <span className="sr-only">Next slide</span>
           </Button>
         </div>
       </div>
 
       {/* Calendar Grid */}
-      <div className="mt-2 grid grid-rows-2 grid-cols-7 gap-3.5 text-center">
+      <div className="mt-2 grid grid-rows-2 grid-cols-7 gap-3 gap-y-0 text-center">
         {/* Weekdays */}
         {daysOfWeek.map((day) => (
-          <div
-            key={day}
-            className="font-medium text-gray-500 p-1.5"
-          >
+          <div key={day} className="font-medium text-gray-500 pt-3">
             {day}
           </div>
         ))}
@@ -74,13 +76,12 @@ const Week = () => {
         {currentWeek.map((date, index) => (
           <button
             key={index}
-            className={`rounded-full  cursor-pointer   ${
+            className={`rounded-full  cursor-pointer pt-0  ${
               date.toDateString() === today.toDateString()
                 ? "bg-gray-800 text-white "
                 : date < today
                 ? "text-gray-400"
                 : "text-gray-700"
-                
             }`}
           >
             {date.getDate()}
--- tsubasa/src/pages/Settings/index.jsx
+++ tsubasa/src/pages/Settings/index.jsx
@@ -1,5 +1,9 @@
-const Settings = () => {
-  return <div>Settings</div>;
-};
+import React from 'react'
 
-export default Settings;
+const Settings = () => {
+  return (
+    <div>Settings</div>
+  )
+}
+
+export default Settings
